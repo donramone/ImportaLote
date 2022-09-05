8c8a1180076e6ccbd72eb2bd4f8c59e6fb07dd62
donramone <luigi_lipreri@hotmail.com>
2022-09-05 12:38:39 -0300

    Update config.js

--- src/Data/config.js
+++ src/Data/config.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 let conexion = mysql.createConnection({
 
-    //host:'18.212.141.183',
-    host:'127.0.0.1',
-    user:'root',
-    password:'luigi',
-    port: 33060,
-    database:'Sogico'
+
+    host:'',
+    user:'',
+    password:'',
+    port: ,
+    database:''
   
   });
 conexion.connect(function(err) {
